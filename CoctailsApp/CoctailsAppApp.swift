7f8cf7c639ed2661ac4e6476d60dfb8227a438d1
Roman <romaholovai@gmail.com>
2024-07-19 13:43:24 +0200

    Initial Commit

---
+++ CoctailsApp/CoctailsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoctailsAppApp.swift
+//  CoctailsApp
+//
+//  Created by Roman on 19/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct CoctailsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
